402888a148733c5352e8c6088520f713470c8cbb
OpDevGang <cryptonomics456@gmail.com>
2022-08-15 17:47:54 -0700

    Server file change

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<p>Gang</p>')
     res.send('Hello World')
-    res.end();
 })
 
 function keepAlive() {
     app.listen(3000, () => { console.log('Server is online!')})
 }
 
-module.exports = keepAlive
+module.exports = keepAlive;
